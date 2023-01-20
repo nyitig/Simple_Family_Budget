857b5c7d78aee0263be677b2895f74163d3cebf9
NyitiG <61630823+nyitig@users.noreply.github.com>
2023-01-20 15:30:36 +0100

    input span widht change, add valid function
    style.css:
    - change span border style
    - change width
    
    app.js:
    - remove jsonTemp
    -username and password verification is complete

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // loginBtn click
-let jsonTemp
 const loginBtn=document.getElementById('loginBtn')
 // login name input
 let loginName=""
@@ -13,7 +12,7 @@ const loginPasswInp=document.getElementById('loginPasswInp')
 loginPasswInp.addEventListener("keyup",()=>{
     password=loginPasswInp.value
 })
-
+// Login button click
 loginBtn.addEventListener('click', (event)=> {
     event.preventDefault();
     fetch("/json/./user.json")
@@ -21,8 +20,29 @@ loginBtn.addEventListener('click', (event)=> {
         return response.json();
     })
     .then(jsondata => {
-        jsonTemp=jsondata
-        console.log(jsondata)
+        let isTrue=false
+        // username and password verification
+        for (let i = 0; i < jsondata.users.length; i++) {
+            if (jsondata.users[i].name==loginName) {
+                if (jsondata.users[i].password==password) {
+                    isTrue=true
+                    console.log("Talált!")
+                    loginNameInp.value=""
+                    loginPasswInp.value=""
+                }
+            }
+        }
+        if (isTrue==false) {
+            loginNameInp.value=""
+            loginPasswInp.value=""
+            const loginINpSpan=document.getElementById('loginINpSpan')
+            loginINpSpan.style.color="red"
+            loginINpSpan.innerHTML="Hibás felhasználónév, jelszó!"
+            setTimeout(() => {
+                loginINpSpan.innerHTML="Felhasználónév"
+                loginINpSpan.removeAttribute("style")
+            }, 3000);
+        }
     }
         );
 })
